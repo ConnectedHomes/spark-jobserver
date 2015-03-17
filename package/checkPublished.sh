53d09beb953db35e375ecd00d09947bf870a8e8e
Saket <saket.kumar@bgch.co.uk>
2015-03-17 17:10:35 +0000

    Adding packaging and publishing files

--- package/checkPublished.sh
+++ package/checkPublished.sh
@@ -2,7 +2,7 @@
 set -e
 SPARK_JOB_SERVER_VERSION=1.0
 echo "SPARK_JOB_SERVER_VERSION=$SPARK_JOB_SERVER_VERSION"
-DEB=spark-job-server-${SPARK_JOB_SERVER_VERSION}-${PIPELINE_NUMBER}_all.deb
+DEB=spark-job-server_${SPARK_JOB_SERVER_VERSION}-${PIPELINE_NUMBER}_all.deb
 
 diditpubli.sh -u apt -p "$APT_PASSWORD" -r release "$DEB"
 

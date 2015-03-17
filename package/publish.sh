5b5009f5d6a78dbdafed4415e923ba5ff484eb14
Saket <saket.kumar@bgch.co.uk>
2015-03-17 16:35:38 +0000

    Adding packaging and publishing files

--- package/publish.sh
+++ package/publish.sh
@@ -2,7 +2,7 @@
 set -e
 SPARK_JOB_SERVER_VERSION=1.0
 echo "SPARK_JOB_SERVER_VERSION=$SPARK_JOB_SERVER_VERSION"
-DEB=etl_${SPARK_JOB_SERVER_VERSION}-${PIPELINE_NUMBER}_all.deb
+DEB=spark-job-server_${SPARK_JOB_SERVER_VERSION}-${PIPELINE_NUMBER}_all.deb
 
 publish-package.sh -r release -u bgchadmin ${DEB}
 
